fe54b415bbd660a6daed99fa1e3f2287f73e1c0d
RAM KUMAR <tcs15b023@iittp.ac.in>
2017-08-25 10:51:43 +0530

    created and tested a music service class(image was made clickable just for testing purpose)

--- app/src/main/java/com/ram/drishti/MusicPlayerActivity.java
+++ app/src/main/java/com/ram/drishti/MusicPlayerActivity.java
@@ -1,6 +1,7 @@
 package com.ram.drishti;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -8,6 +9,7 @@ import android.graphics.Color;
 import android.media.MediaMetadataRetriever;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,16 +28,19 @@ public class MusicPlayerActivity extends AppCompatActivity {
     TextView album,genre,artist,songname;
     ImageView album_art;
     String path[];
+    public static final String ACTION_STRING = "SONG_COMPLETE";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
-        //
+
+        //linking various UI elements defined in xml page
         album=(TextView)findViewById(R.id.album);
         artist=(TextView)findViewById(R.id.artist);
         genre=(TextView)findViewById(R.id.genre);
         album_art=(ImageView)findViewById(R.id.album_art);
         songname=(TextView)findViewById(R.id.songname);
+
         //getting list of music files and if not already there searching the device for it
         musicdata=getSharedPreferences("musicdata", Context.MODE_PRIVATE);
         musicdataeditor=musicdata.edit();
@@ -71,6 +76,19 @@ public class MusicPlayerActivity extends AppCompatActivity {
         musicdataeditor.putBoolean("isShuffling",isShuffling);
         musicdataeditor.commit();
         setScreen(position);
+        album_art.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                position=musicdata.getInt("position",0);
+                int pos=position+1;
+                if(pos>=mp3path.size())pos=0;
+                musicdataeditor.putInt("position",pos);
+                musicdataeditor.commit();
+                stopService(new Intent(MusicPlayerActivity.this, MusicService.class));
+                startService(new Intent(MusicPlayerActivity.this,MusicService.class));
+                setScreen(pos);
+            }
+        });
 
     }
 
---
+++ app/src/main/java/com/ram/drishti/MusicService.java
@@ -0,0 +1,98 @@
+package com.ram.drishti;
+
+import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.IBinder;
+
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Set;
+
+public class MusicService extends Service implements MediaPlayer.OnCompletionListener {
+    SharedPreferences musicdata;
+    SharedPreferences.Editor musicdataeditor;
+    Set<String> mp3path=new HashSet<String>();
+    String[] path;
+    int position;
+    boolean isPlaying=false;
+    boolean isLooping;
+    boolean isShuffling;
+    MediaPlayer player;
+    public MusicService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        musicdata=getSharedPreferences("musicdata", Context.MODE_PRIVATE);
+        musicdataeditor=musicdata.edit();
+        mp3path=musicdata.getStringSet("mp3path",null);
+        if(mp3path.size()<=0){
+            onDestroy();
+        }
+        path=new String[mp3path.size()];
+        mp3path.toArray(path);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        position=musicdata.getInt("position", 0);
+        if(position>=mp3path.size()||position<0){
+            position=0;
+        }
+        player=MediaPlayer.create(this, Uri.parse(path[position]));
+        player.setOnCompletionListener(this);
+        if (player.isPlaying()){
+            player.stop();
+        }
+        player.start();
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        if(player.isPlaying())player.stop();
+    }
+
+    @Override
+    public void onCompletion(MediaPlayer mp) {
+        isLooping=musicdata.getBoolean("isLooping",false);
+        isShuffling=musicdata.getBoolean("isShuffling",false);
+        if(!isLooping){
+            if(isShuffling){
+                Random random=new Random();
+                position=random.nextInt(mp3path.size());
+            }
+            else{
+                if(position==mp3path.size()-1){
+                    position=0;
+                }
+                else{
+                    position=position+1;
+                }
+            }
+            musicdataeditor.putInt("position",position);
+            musicdataeditor.commit();
+        }
+        sendBroadcast(new Intent(MusicPlayerActivity.ACTION_STRING));
+        player.reset();
+        try {
+            player.setDataSource(this,Uri.parse(path[position]));
+            player.start();
+        }catch (Exception e){
+            stopService(new Intent(this,MusicService.class));
+            startService(new Intent(this,MusicService.class));
+        }
+    }
+}

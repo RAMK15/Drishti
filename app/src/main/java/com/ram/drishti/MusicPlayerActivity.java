0432b975fcccf011a799843b2f80cabb004df286
RAM KUMAR <tcs15b023@iittp.ac.in>
2017-08-25 12:36:31 +0530

    added basic gesture controls to musicplayeractivity(swipes,taps,double taps).Left long press for voice interaction

--- app/src/main/java/com/ram/drishti/MusicPlayerActivity.java
+++ app/src/main/java/com/ram/drishti/MusicPlayerActivity.java
@@ -1,23 +1,30 @@
 package com.ram.drishti;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.media.MediaMetadataRetriever;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
-public class MusicPlayerActivity extends AppCompatActivity {
+public class MusicPlayerActivity extends AppCompatActivity implements GestureDetector.OnGestureListener,GestureDetector.OnDoubleTapListener {
     SharedPreferences musicdata;
     SharedPreferences.Editor musicdataeditor;
     Set<String> mp3path=new HashSet<String>();
@@ -29,6 +36,16 @@ public class MusicPlayerActivity extends AppCompatActivity {
     ImageView album_art;
     String path[];
     public static final String ACTION_STRING = "SONG_COMPLETE";
+    private GestureDetectorCompat mDetector;
+    private VelocityTracker mVelocityTracker = null;
+
+    private BroadcastReceiver receiver=new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            position=musicdata.getInt("position",0);
+            setScreen(position);
+        }
+    };
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -73,25 +90,124 @@ public class MusicPlayerActivity extends AppCompatActivity {
         isLooping=musicdata.getBoolean("isLooping",false);
         isShuffling=musicdata.getBoolean("isShuffling",false);
         musicdataeditor.putBoolean("isLooping",isLooping);
-        musicdataeditor.putBoolean("isShuffling",isShuffling);
+        musicdataeditor.putBoolean("isShuffling", isShuffling);
         musicdataeditor.commit();
         setScreen(position);
-        album_art.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                position=musicdata.getInt("position",0);
-                int pos=position+1;
-                if(pos>=mp3path.size())pos=0;
-                musicdataeditor.putInt("position",pos);
-                musicdataeditor.commit();
-                stopService(new Intent(MusicPlayerActivity.this, MusicService.class));
-                startService(new Intent(MusicPlayerActivity.this,MusicService.class));
-                setScreen(pos);
-            }
-        });
+        mDetector=new GestureDetectorCompat(this,this);
+        mDetector.setOnDoubleTapListener(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(receiver);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(receiver, new IntentFilter(ACTION_STRING));
+    }
+
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        this.mDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    public boolean onSingleTapConfirmed(MotionEvent e) {
+        Intent i=new Intent(MusicPlayerActivity.this,MusicService.class);
+        isPlaying=musicdata.getBoolean("isPlaying",false);
+        if(isPlaying){
+            stopService(i);
+            isPlaying=false;
+        }
+        else{
+            startService(i);
+            isPlaying=true;
+        }
+        musicdataeditor.putBoolean("isPlaying", isPlaying);
+        musicdataeditor.commit();
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTap(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onDoubleTapEvent(MotionEvent e) {
+        Intent i = new Intent(MusicPlayerActivity.this, MusicService.class);
+        stopService(i);
+        finish();
+        return true;
+    }
+
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
 
     }
 
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return true;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent event1, MotionEvent event2, float velocityX, float velocityY) {
+        int index1 = event1.getActionIndex();
+        int pointerId1 = event1.getPointerId(index1);
+        int index2 = event2.getActionIndex();
+        int pointerId2 = event2.getPointerId(index2);
+        double x1=event1.getX(pointerId1);
+        double y1=event1.getY(pointerId1);
+        double x2=event2.getX(pointerId2);
+        double y2=event2.getY(pointerId2);
+        double theta=Math.toDegrees(Math.atan2(y1-y2,x2-x1));
+        if(x2-x1<0 && (theta>135 && theta<=180) || (theta<-135 && theta>=-180)){
+            //left swipe for next song
+            playnextsong();
+        }
+        else if(x2-x1>0 && theta<45 && theta>-45){
+            playprevsong();
+            //right swipe for prev song
+        }
+        else if(y2-y1<0 && theta>45 && theta<135){
+            //up for shuffling
+            isShuffling=musicdata.getBoolean("isShuffling",false);
+            musicdataeditor.putBoolean("isShuffling",(!isShuffling));
+            musicdataeditor.commit();
+        }
+        else if(y2-y1>0 && theta<-45 && theta>-135){
+            //down for looping
+            isLooping=musicdata.getBoolean("isLooping",false);
+            musicdataeditor.putBoolean("isLooping",(!isLooping));
+            musicdataeditor.commit();
+        }
+        else{
+            //some error
+        }
+        return true;
+    }
     public  void setScreen(int position){
         MediaMetadataRetriever metaRetriver;
         metaRetriver = new MediaMetadataRetriever();
@@ -119,5 +235,50 @@ public class MusicPlayerActivity extends AppCompatActivity {
             artist.setText("Unknown Artist");
             genre.setText("Unknown Genre");
         }
+    }
+    public  void playnextsong(){
+        Intent i=new Intent(MusicPlayerActivity.this,MusicService.class);
+        isLooping=musicdata.getBoolean("isLooping", false);
+        isShuffling=musicdata.getBoolean("isShuffling",false);
+        if(isShuffling){
+            Random random=new Random();
+            position=random.nextInt(mp3path.size());
+        }
+        else{
+            if(position==mp3path.size()-1){
+                position=0;
+            }
+            else{
+                position=position+1;
+            }
+        }
+        musicdataeditor.putInt("position",position);
+        musicdataeditor.commit();
+        stopService(i);
+        startService(i);
+        setScreen(position);
+
+    }
+    public  void playprevsong(){
+        Intent i=new Intent(MusicPlayerActivity.this,MusicService.class);
+        isLooping=musicdata.getBoolean("isLooping",false);
+        isShuffling=musicdata.getBoolean("isShuffling",false);
+        if(isShuffling){
+            Random random=new Random();
+            position=random.nextInt(mp3path.size());
+        }
+        else{
+            if(position==0){
+                position=mp3path.size()-1;
+            }
+            else{
+                position=position-1;
+            }
+        }
+        musicdataeditor.putInt("position",position);
+        musicdataeditor.commit();
+        stopService(i);
+        startService(i);
+        setScreen(position);
     }
 }
